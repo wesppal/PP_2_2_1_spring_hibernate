f4143a4ad9432fe7500e39ab55d0ee89e290dc5f
Pavel Ivanchik <ivanchik.pal@gmail.com>
2022-07-19 22:33:57 +0300

    C1

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -2,7 +2,6 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
